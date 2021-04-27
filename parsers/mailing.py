ebb92b62b6c77a0f08c582c06474423f82a4d9c2
ProgerGagik <gagikgrigoryan2005@mail.ru>
2021-04-27 18:07:24 +0300

    mailing added

--- parsers/mailing.py
+++ parsers/mailing.py
@@ -1,29 +1,36 @@
 from data.db_session import global_init, create_session
 from data.users import User
+from data.Sales import Sales
 import smtplib
-import email.message
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 server = smtplib.SMTP('smtp.gmail.com:587')
+PASSWORD = 'password'
+server.starttls()
+server.login('mail', PASSWORD)
 
 
-def send_mail(user_list, game):
-    global_init('db/games.db')
-
-    email_content = """ HTML """
-
-    msg = email.message.Message()
-    msg['Subject'] = 'Sale on following game'
-    msg['From'] = 'youraddress'
-    msg['To'] = 'to_address'
-    password = "yourpassword"
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(email_content)
-
-    server.starttls()
-    server.login(msg['From'], password)
-    server.sendmail(msg['From'], [msg['To']], msg.as_string())
-
+def send_mail():
+    global_init('../db/games.db')
     db_sess = create_session()
-    user_list = [1, 2, 4]
-    for i in user_list:
-        user = db_sess.query(User).filter((User.id == i)).first()
+    games_ = db_sess.query(Sales).filter().all()
+    try:
+        for game_ in games_:
+            if game_.game.foll_profiles:
+                profiles = game_.game.foll_profiles.split(', ')
+                profiles = list(map(lambda x: int(x), profiles))
+                users = db_sess.query(User).filter(User.id.in_(profiles)).all()
+                for us in users:
+                    msg = MIMEMultipart()
+                    msg['Subject'] = 'Sale on following game'
+                    msg['From'] = 'mail'
+                    msg['To'] = us.email
+                    message = f'There is sale on your following game {game_.game.name}. ' \
+                              f'Visit our site to see more information.'
+                    msg.attach(MIMEText(message, 'plain'))
+                    server.sendmail(msg['From'], [msg['To']], msg.as_string())
+    except Exception as err:
+        print(err)
+
+send_mail()
